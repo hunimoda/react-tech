cd998ae000642592af5935cb3f2b807fd9f1e785
hunimoda <hunimoda@gmail.com>
2022-04-11 00:23:34 +0900

    Common component with Route, Outlet

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
 import Article from "./pages/Article";
 import Articles from "./pages/Articles";
 import Home from "./pages/Home";
@@ -9,9 +10,11 @@ import "./App.css";
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/about" element={<About />} />
-			<Route path="/profiles/:username" element={<Profile />} />
+			<Route element={<Layout />}>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/profiles/:username" element={<Profile />} />
+			</Route>
 			<Route path="/articles" element={<Articles />}>
 				<Route path=":id" element={<Article />} />
 			</Route>
