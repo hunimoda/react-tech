107d62b289978061b2f1140b29484e4f01a5c958
hunimoda <hunimoda@gmail.com>
2022-04-11 00:06:18 +0900

    Nested route

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,4 +1,6 @@
 import { Route, Routes } from "react-router-dom";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
@@ -10,6 +12,9 @@ function App() {
 			<Route path="/" element={<Home />} />
 			<Route path="/about" element={<About />} />
 			<Route path="/profiles/:username" element={<Profile />} />
+			<Route path="/articles" element={<Articles />}>
+				<Route path=":id" element={<Article />} />
+			</Route>
 		</Routes>
 	);
 }
